3679ae7b737fa500ec566e094ba85de0b13507e7
Payal <payalkumari255555@gmail.com>
2021-06-28 20:46:06 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -12,33 +12,47 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String dropdownValue = '2021';
+  String dropdownYear = '2021';
   String dropDownMonth = 'May';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.black54,
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              Text(
-                'Customize',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold,
+              Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                        top: 100
+                    ),
+                    child: Text(
+                      'Customize',
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  child: Text(
+                    'When are you Planning your GRE?',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
-              Text(
-                'When are you Planning your GRE?',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 30.0,
+              SizedBox(height: 50.0,
               ),
               Container(
                 padding: EdgeInsets.all(10.0),
@@ -49,7 +63,7 @@ class _MyAppState extends State<MyApp> {
                   children: [
                     Icon(
                       Icons.alarm_on_rounded,
-                      color: Colors.teal,
+                      color: Colors.blueGrey,
                     ),
                     SizedBox(
                       width: 10.0,
@@ -57,7 +71,7 @@ class _MyAppState extends State<MyApp> {
                     Text(
                       '3 Months',
                       style: TextStyle(
-                        color: Colors.teal.shade900,
+                        color: Colors.blueGrey,
                         fontFamily: 'Source Sans Pro',
                         fontSize: 20.0,
                       ),
@@ -74,7 +88,7 @@ class _MyAppState extends State<MyApp> {
                   children: [
                     Icon(
                       Icons.alarm_on_rounded,
-                      color: Colors.teal,
+                      color: Colors.blueGrey,
                     ),
                     SizedBox(
                       width: 10.0,
@@ -82,7 +96,7 @@ class _MyAppState extends State<MyApp> {
                     Text(
                       '6 Months',
                       style: TextStyle(
-                        color: Colors.teal.shade900,
+                        color: Colors.blueGrey,
                         fontFamily: 'Source Sans Pro',
                         fontSize: 20.0,
                       ),
@@ -99,7 +113,7 @@ class _MyAppState extends State<MyApp> {
                   children: [
                     Icon(
                       Icons.alarm_on_rounded,
-                      color: Colors.teal,
+                      color: Colors.blueGrey,
                     ),
                     SizedBox(
                       width: 10.0,
@@ -107,7 +121,7 @@ class _MyAppState extends State<MyApp> {
                     Text(
                       '9 Months',
                       style: TextStyle(
-                        color: Colors.teal.shade900,
+                        color: Colors.blueGrey,
                         fontFamily: 'Source Sans Pro',
                         fontSize: 20.0,
                       ),
@@ -124,7 +138,7 @@ class _MyAppState extends State<MyApp> {
                   children: [
                     Icon(
                       Icons.alarm_on_rounded,
-                      color: Colors.teal,
+                      color: Colors.blueGrey,
                     ),
                     SizedBox(
                       width: 10.0,
@@ -132,9 +146,10 @@ class _MyAppState extends State<MyApp> {
                     Text(
                       '12 Months',
                       style: TextStyle(
-                        color: Colors.teal.shade900,
+                        color: Colors.blueGrey,
                         fontFamily: 'Source Sans Pro',
                         fontSize: 20.0,
+
                       ),
                     )
                   ],
@@ -142,86 +157,145 @@ class _MyAppState extends State<MyApp> {
               ),
               SizedBox(height: 20.0,
               ),
-              Text('(OR)',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.blue,
-                  fontWeight: FontWeight.bold,
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text('(OR)',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                  ),
                 ),
+              ),
+              SizedBox(height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text('Months',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                        ),
+                        child: DropdownButton<String>(
+                          value: dropDownMonth,
+                          icon: const Icon(Icons.arrow_drop_down_outlined),
+                          isExpanded: true,
+                          iconSize: 30,
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.blueGrey),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropDownMonth = newValue!;
+                            });
+                          },
+                          items: <String>[
+                            'May',
+                            'June',
+                            'July'
+                          ].map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    letterSpacing: 0.9,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text('Months',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                        ),
+                        child: DropdownButton<String>(
+                          value: dropdownYear,
+                          icon: const Icon(Icons.arrow_drop_down_outlined),
+                          isExpanded: true,
+                          iconSize: 30,
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.blueGrey),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownYear = newValue!;
+                            });
+                          },
+                          items: <String>[
+                            '2021',
+                            '2022',
+                            '2023'
+                          ].map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    letterSpacing: 0.9,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
 
               Container(
-                child: DropdownButton<String>(
-                  value: dropDownMonth,
-                  icon: const Icon(Icons.arrow_drop_down_outlined),
-                  isExpanded: true,
-                  iconSize: 30,
-                  elevation: 20,
-                  style: const TextStyle(color: Colors.black),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      dropDownMonth = newValue!;
-                    });
-                  },
-                  items: <String>[
-                    'May',
-                    'June',
-                    'July',
-                    'August'
-                  ].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
+                  child: Container(
+                    margin: EdgeInsets.all(30),
+                    padding: EdgeInsets.all(20),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Color(0xFF2B6EB1),
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Center(
                       child: Text(
-                        value,
+                        "Almost Done",
                         style: TextStyle(
-                            fontSize: 20,
-                            letterSpacing: 0.9,
-                            fontWeight: FontWeight.w300),
+                            color: Colors.white,
+                            fontSize: 20
+                        ),
                       ),
-                    );
-                  }).toList(),
+                    ),
+                  ),
                 ),
-              ),
-
-              Container(
-                child: DropdownButton<String>(
-                  value: dropdownValue,
-                  icon: const Icon(Icons.arrow_drop_down_outlined),
-                  isExpanded: true,
-                  iconSize: 30,
-                  elevation: 20,
-                  style: const TextStyle(color: Colors.black),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      dropdownValue = newValue!;
-                    });
-                  },
-                  items: <String>[
-                    '2021',
-                    '2022',
-                    '2023',
-                    '2024'
-                  ].map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(
-                        value,
-                        style: TextStyle(
-                            fontSize: 20,
-                            letterSpacing: 0.9,
-                            fontWeight: FontWeight.w300),
-                      ),
-                    );
-                  }).toList(),
-                ),
-              ),
-              RaisedButton(
-                color: Colors.blue, // background
-                textColor: Colors.white, // foreground
-                onPressed: () { },
-                child: Text('Almost Done'),
-              )
-
             ],
           ),
         ),
